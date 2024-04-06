402d5431d5de93ac10ef532bca6a7db12415c17b
Matt Simerson <matt@tnpi.net>
2024-04-06 12:40:21 -0700

    fix: workaround for npm cli bug #3611

--- src/helpers/npmTags.js
+++ src/helpers/npmTags.js
@@ -6,10 +6,12 @@ module.exports = {
 };
 
 function getNpmTags(dependency, logger) {
-    const command = `npm view ${dependency.name} versions`;
+    const command = `npm view ${dependency.name} versions --json`;
 
     return minions.spawn(command, logger).then(result => {
-        const strippedColors = result.replace(ansiRegex, "").replace(/'/g, '"');
+        let strippedColors = result.replace(ansiRegex, "").replace(/'/g, '"');
+        // workaround for bug npm/cli#3611
+        if (!/\[/.test(strippedColors)) strippedColors = `[ ${strippedColors.trim()} ]`
 
         try {
             return JSON.parse(strippedColors);
